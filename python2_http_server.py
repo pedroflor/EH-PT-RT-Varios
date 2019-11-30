0c508305492966fe1d498821f8823b2ac1942ccc
PF <pedro.flor@gmail.com>
2019-11-29 20:41:26 -0400

    First commit

---
+++ python2_http_server.py
@@ -0,0 +1,6 @@
+"""
+1) Ubicarse en el directorio que se desea publicar contenido.
+2) Ejecutar desde linea de comandos ó
+3) Convertir a .exe y ejecutar en un Windows SIN python.
+"""
+python2 -c 'import SimpleHTTPServer,SocketServer;PORT = 8000; Handler = SimpleHTTPServer.SimpleHTTPRequestHandler; httpd = SocketServer.TCPServer(("", PORT), Handler); print "serving at port", PORT; httpd.serve_forever()'
