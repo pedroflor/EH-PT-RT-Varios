d826104859fd6a612381c0a0d44e1fe4bcaa837f
PF <pedro.flor@gmail.com>
2021-07-18 10:49:43 -0400

    Update debian2kali.sh

--- debian2kali.sh
+++ debian2kali.sh
@@ -5,7 +5,8 @@ apt -y install wget gnupg dirmngr
 wget -q -O - https://archive.kali.org/archive-key.asc | gpg --import
 gpg --keyserver keyserver.ubuntu.com --recv-key 44C6513A8E4FB3D30875F758ED444FF07D8D0BF6
 echo "deb http://http.kali.org/kali kali-rolling main non-free contrib" >> /etc/apt/sources.list
-echo "deb http://http.kali.org/kali kali-last-snapshot main non-free contrib" >> /etc/apt/sources.list
+### OPCIONAL 
+### echo "deb http://http.kali.org/kali kali-last-snapshot main non-free contrib" >> /etc/apt/sources.list
 gpg -a --export ED444FF07D8D0BF6 | apt-key add -
 
 # Editar sources.list y dejar únicamente Kali Repos
