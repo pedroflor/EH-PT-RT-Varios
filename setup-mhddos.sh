a342d35c92e59d29ec0f7d7d2965230351318790
PF <pedro.flor@gmail.com>
2024-02-17 20:33:29 -0400

    Update setup-mhddos.sh

--- setup-mhddos.sh
+++ setup-mhddos.sh
@@ -10,7 +10,7 @@ cd
 git clone https://github.com/MatrixTM/MHDDoS.git
 cd MHDDoS
 pip3 install -r requirements.txt --break-system-packages
-
+pip3 install -r requirements.txt --break-system-packages
 
 
 ## Create user
